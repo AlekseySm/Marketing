72f713c9367dc5d9c8103da3b71479abc9a46354
ol-stan <olstan125@gmail.com>
2020-05-28 22:19:04 +0300

    change and fix 28052020 part1

--- head.php
+++ head.php
@@ -1,47 +1,40 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+      <meta charset="<?php bloginfo('charset'); ?>">
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-    <meta name="keywords" content="">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="---">
-    <meta property="og:description" content="---">
-    <meta property="og:image" content="---">
-    <meta property="og:url" content="---">
-    <meta name="twitter:image" content="---">
-    <meta name="twitter:title" content="---">
-    <meta name="twitter:url" content="---">
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:description" content="---">
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+      <meta name="keywords" content="">
+      <meta property="og:type" content="website">
+      <meta property="og:title" content="---">
+      <meta property="og:description" content="---">
+      <meta property="og:image" content="---">
+      <meta property="og:url" content="---">
+      <meta name="twitter:image" content="---">
+      <meta name="twitter:title" content="---">
+      <meta name="twitter:url" content="---">
+      <meta name="twitter:card" content="summary">
+      <meta name="twitter:description" content="---">
 
-  <!-- Favicons -->
-  <link rel="apple-touch-icon" sizes="180x180"
-        href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32"
-        href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="194x194"
-        href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-194x194.png">
-  <link rel="icon" type="image/png" sizes="192x192"
-        href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/android-chrome-192x192.png">
-  <link rel="icon" type="image/png" sizes="16x16"
-        href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-16x16.png">
-  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/site.webmanifest">
-  <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/safari-pinned-tab.svg"
-        color="#ee9537">
-<!--  <link rel="shortcut icon" href="--><?php //echo get_template_directory_uri(); ?><!--/build/images/favicons/favicon.ico">-->
-  <meta name="apple-mobile-web-app-title" content="4eck Theme">
-  <meta name="application-name" content="4eck Theme">
-  <meta name="msapplication-TileColor" content="#00aba9">
-  <meta name="msapplication-TileImage"
-        content="<?php echo get_template_directory_uri(); ?>/build/images/favicons/mstile-144x144.png">
-  <meta name="msapplication-config"
-        content="<?php echo get_template_directory_uri(); ?>/build/images/favicons/browserconfig.xml">
-  <meta name="theme-color" content="#ffffff">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Open+Sans:wght@400;600;700&family=Roboto&display=swap"
-        rel="stylesheet">
-    <?php wp_head(); ?>
+      <!-- Favicons -->
+      <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="194x194" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-194x194.png">
+      <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/android-chrome-192x192.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/favicon-16x16.png">
+      <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/site.webmanifest">
+      <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/build/images/favicons/safari-pinned-tab.svg" color="#ee9537">
+      <!--  <link rel="shortcut icon" href="--><?php //echo get_template_directory_uri(); 
+                                                ?>
+      <!--/build/images/favicons/favicon.ico">-->
+      <meta name="apple-mobile-web-app-title" content="4eck Theme">
+      <meta name="application-name" content="4eck Theme">
+      <meta name="msapplication-TileColor" content="#00aba9">
+      <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/build/images/favicons/mstile-144x144.png">
+      <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/build/images/favicons/browserconfig.xml">
+      <meta name="theme-color" content="#ffffff">
+      <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Open+Sans:wght@400;600;700" rel="stylesheet">
+      <?php wp_head(); ?>
 </head>
